ba01ca57aabfed22e318d5c7527f0cc691d0cdb2
Ryan Anderson <ryan.anderson@buzzuti.com>
2015-06-26 10:51:57 -0500

    testing countdown timer.

--- src/demoweb/js/custom.js
+++ src/demoweb/js/custom.js
@@ -52,7 +52,7 @@ $(document).ready(function () {
     /* ==============================================
        Countdown
     =============================================== */
-    var launchDay = new Date(2015, 6, 1);
+    var launchDay = new Date(2015, 5, 1);
     $('#timer').countdown({
         until: launchDay
     });
